462eb57731114344bb3a5a5b8251768f4b1c69c1
Aleksandr <aleksandrnosov2008@gmail.com>
2023-11-20 23:48:51 +0300

    create burger + slider

--- main.js
+++ main.js
@@ -1,25 +1,61 @@
 $(function() {
     let header = $("#header"),
-        headerH,
         nav = $("#nav"),
-        navH,
-        intro = $("#intro"),
-        introH,
+        navH = nav.innerHeight(),
+        features = $("#features"),
+        featuresH = features.innerHeight(),
+        works = $("#works"),
+        burger = $("#burger"),
         scrollPos = $(window).scrollTop();
+
+        checkScroll(scrollPos, navH, featuresH);
     
-    $(window).on("scroll load", function() {
-        headerH = header.innerHeight();
+    // CheckScroll / HeaderFixed
+
+    $(window).on("scroll resize", function() {
         navH = nav.innerHeight();
-        introH = intro.innerHeight();
+        featuresH = features.innerHeight(),
         scrollPos = $(this).scrollTop();
+        checkScroll(scrollPos, navH, featuresH);
+    });
 
 
+
+    function checkScroll(scrollPos, navH, featuresH) {
         if(scrollPos > navH) {
-            header.addClass("fixed");    
+            header.addClass("fixed");  
         } else {
             header.removeClass("fixed")
         }
 
-    })
+        // if(scrollPos > featuresH) {
+        //     works.addClass("show")
+        // } else {
+        //     works.removeClass("show")
+        // }
+    }
 
+    // burgerMenu
+        burger.on("click ", function(event) {
+            event.preventDefault();
+            nav.toggleClass("active");
+            burger.toggleClass("active")
+        });
+    
+        nav.on("click", function() {
+            nav.removeClass("active");
+            burger.removeClass("active");
+        }); 
+
+    // Reviews https://kenwheeler.github.io/slick/
+
+    let slider = $("#reviewsSlider");
+    slider.slick({
+        infinite: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        arrows: false,
+        dots: true
+      });
 });
